ae83f577df99a80102dcdc36e9808aa0a2afeb5d
Bruno <garcia.bruno0@gmail.com>
2017-03-31 13:38:24 -0300

    Added support for URL conversion instead of profile regex matches

--- core/bot.py
+++ core/bot.py
@@ -10,6 +10,7 @@ import datetime
 
 import steam.guard
 import steam.webauth
+from steam import SteamID
 from steam.enums import EResult
 
 import enums
@@ -828,18 +829,8 @@ class DeliveryBot(object):
                 )
             )
 
-            sender_steam_id_matches = re.findall(
-                r'http://steamcommunity\.com/profiles/([0-9]+)',
-                gift.from_link,
-                re.DOTALL
-            )
-
-            if not len(sender_steam_id_matches):
-                log.error(u'Regex failed to retrieve steam sender id')
-
-                continue
-
-            sender_steam_id = sender_steam_id_matches[0]
+            sender = SteamID.from_url(gift.from_link)
+            sender_steam_id = sender.as_64
 
             if not gift.accept_button or 'UnpackGift' in gift.accept_button:
                 log.info(u'Gift cannot be accepted to inventory')
